611b3606cfd523babad4a21f64e9c99500b98004
gabriel <gabriel.goulart@ibridge.com.br>
2018-11-29 00:14:17 -0200

    print do llcode

--- src/main/Main.java
+++ src/main/Main.java
@@ -25,7 +25,7 @@ public class Main {
 
 	public static SymbolTable parse(String[] args, String filepath) throws Exception {
 
-		String inputFile = "/home/thomas/Desktop/faculdade/compiladores/INE5426/src/testes/programa_teste_classe";
+		String inputFile = "/home/gabriel/Documentos/UFSC/2018.2/compiladores/t4/INE5426/src/testes/programa_teste_classe";
 		if (args.length > 0) {
 			inputFile = filepath + args[0];
 			int endIndex = inputFile.lastIndexOf("/");
@@ -67,6 +67,7 @@ public class Main {
 		ParseTreeWalker walker = new ParseTreeWalker();
 		analisadorSemanticListener semanticListener = new analisadorSemanticListener(filepath);
 		walker.walk(semanticListener, tree);
+		System.out.println(semanticListener.getllcode());
 		return semanticListener.getSymbolTable();
 
 	}
